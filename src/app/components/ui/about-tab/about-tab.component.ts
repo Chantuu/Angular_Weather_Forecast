215f95863a26fd0456a0f8785abd9f25d2930ae8
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 02:06:45 +0400

    Create AboutTabComponent
     Created this component to prevent SidebarContainerComponent from cluttering up.

---
+++ src/app/components/ui/about-tab/about-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-tab',
+  imports: [],
+  templateUrl: './about-tab.component.html',
+  styleUrl: './about-tab.component.css'
+})
+export class AboutTabComponent {
+
+}
