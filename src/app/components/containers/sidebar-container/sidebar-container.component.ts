d31908735c1b845c9bec0293fbf6f14a2d3e40b1
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-16 16:35:05 +0400

    Import SidebarButtonContainerComponent in SidebarContainerComponent

--- src/app/components/containers/sidebar-container/sidebar-container.component.ts
+++ src/app/components/containers/sidebar-container/sidebar-container.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {SidebarButtonContainerComponent} from "../sidebar-button-container/sidebar-button-container.component";
 
 @Component({
   selector: 'app-sidebar-container',
-  imports: [],
+  imports: [SidebarButtonContainerComponent],
   templateUrl: './sidebar-container.component.html',
   styleUrl: './sidebar-container.component.css'
 })
