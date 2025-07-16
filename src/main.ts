70429997d63b5913aa18160e8dbd507b793a2023
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-16 15:13:34 +0400

    Restructure Project
     Restructured project to follow angular's practices and generate new components efficiently.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './components/app/app.config';
-import { AppComponent } from './components/app/app.component';
+import { appConfig } from './app/root/app.config';
+import { AppComponent } from './app/root/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
