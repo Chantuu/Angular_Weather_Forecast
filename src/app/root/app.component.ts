f2dc025a14221f5b17a82f5fd3f5c34a1109b9da
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-16 15:55:23 +0400

    Add Container Components Inside App Component

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {SidebarContainerComponent} from "../components/containers/sidebar-container/sidebar-container.component";
+import {MainContainerComponent} from "../components/containers/main-container/main-container.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    SidebarContainerComponent,
+    MainContainerComponent
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
