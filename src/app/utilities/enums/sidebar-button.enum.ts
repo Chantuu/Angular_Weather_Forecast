a5b9dc40f4148b338b2078f9a76ff6d136d1043c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-16 21:50:45 +0400

    Add SidebarButtonEnum
     Added this enum to allow functionality of programmatic identification of the sidebar buttons for the parent containers.

---
+++ src/app/utilities/enums/sidebar-button.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * This enum is responsible for telling parent component, which sidebar button
+ * was clicked.
+ */
+export enum SidebarButtonType {
+    Weather = 0,
+    Cities = 1,
+    About = 2
+}
