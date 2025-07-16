1e4de0e3a71682ad224ae2db22efadedd47020d9
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 02:23:37 +0400

    Create WeatherTabComponent
     Created this component to isolate weather display functionality and avoid cluttering SidebarContainerComponent.

---
+++ src/app/components/ui/weather-tab/weather-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-tab',
+  imports: [],
+  templateUrl: './weather-tab.component.html',
+  styleUrl: './weather-tab.component.css'
+})
+export class WeatherTabComponent {
+
+}
