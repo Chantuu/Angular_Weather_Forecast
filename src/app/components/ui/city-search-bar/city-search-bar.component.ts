d5806669eeab4ad0b6a486d01f54d2f8c10795b2
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 02:29:56 +0400

    Create CitySearchBarComponent

---
+++ src/app/components/ui/city-search-bar/city-search-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-search-bar',
+  imports: [],
+  templateUrl: './city-search-bar.component.html',
+  styleUrl: './city-search-bar.component.css'
+})
+export class CitySearchBarComponent {
+
+}
