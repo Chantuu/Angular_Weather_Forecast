d488dda7b623c651599ccc4644ce2378c092b4c7
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 17:32:12 +0400

    Create SidebarWeatherInfoContainer

---
+++ src/app/components/ui/sidebar-weather-info/sidebar-weather-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-weather-info',
+  imports: [],
+  templateUrl: './sidebar-weather-info.component.html',
+  styleUrl: './sidebar-weather-info.component.css'
+})
+export class SidebarWeatherInfoComponent {
+
+}
