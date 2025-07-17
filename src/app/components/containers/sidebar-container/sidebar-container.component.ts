e17f84a60c4dff8e25ad9c2fe287fd73e763e3b6
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 18:35:15 +0400

    Implement Basic SidebarContainerComponent Functionality

--- src/app/components/containers/sidebar-container/sidebar-container.component.ts
+++ src/app/components/containers/sidebar-container/sidebar-container.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import {SidebarButtonContainerComponent} from "../sidebar-button-container/sidebar-button-container.component";
+import {Component, signal} from '@angular/core';
+import {SidebarButtonComponent} from "../../ui/sidebar-button/sidebar-button.component";
+import {SidebarButtonType} from "../../../utilities/enums/sidebar-button.enum";
+import {WeatherTabComponent} from "../../ui/weather-tab/weather-tab.component";
+import {AboutTabComponent} from "../../ui/about-tab/about-tab.component";
 
 @Component({
   selector: 'app-sidebar-container',
-  imports: [SidebarButtonContainerComponent],
+  imports: [SidebarButtonComponent, WeatherTabComponent, AboutTabComponent],
   templateUrl: './sidebar-container.component.html',
   styleUrl: './sidebar-container.component.css'
 })
 export class SidebarContainerComponent {
+  // This enum property was declared to be used inside component template to properly manage sidebar button components.
+  readonly SidebarButtonType = SidebarButtonType;
 
+  /**
+   * This Signal saves which sidebar button was clicked to display corresponding tab in the sidebar.
+   */
+  currentSidebarButtonPressed = signal<SidebarButtonType>(SidebarButtonType.Weather);
+
+  /**
+   * This function is responsible for updating currentSidebarButtonPressed signal which allows to switch between
+   * different tabs in the sidebar.
+   *
+   * @param currentButtonPressed - Parameter containing which sidebar button was clicked
+   */
+  setActiveSidebarButton(currentButtonPressed: SidebarButtonType) {
+    this.currentSidebarButtonPressed.set(currentButtonPressed);
+  }
 }
