3acd03f762867e9d1905f2232ef16a0014778bbc
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 18:16:55 +0400

    Implement Basic WeatherTabComponent Design

--- src/app/components/ui/weather-tab/weather-tab.component.ts
+++ src/app/components/ui/weather-tab/weather-tab.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {CitySearchBarComponent} from "../city-search-bar/city-search-bar.component";
+import {SidebarWeatherInfoComponent} from "../sidebar-weather-info/sidebar-weather-info.component";
 
 @Component({
   selector: 'app-weather-tab',
-  imports: [],
+  imports: [CitySearchBarComponent, SidebarWeatherInfoComponent],
   templateUrl: './weather-tab.component.html',
   styleUrl: './weather-tab.component.css'
 })
