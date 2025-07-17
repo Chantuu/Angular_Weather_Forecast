aec339a85ec480b5caccd02d3f3cfe19354fd624
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-17 18:03:17 +0400

    Implement SidebarWeatherInfoComponent Functionality

--- src/app/components/ui/sidebar-weather-info/sidebar-weather-info.component.ts
+++ src/app/components/ui/sidebar-weather-info/sidebar-weather-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-weather-info',
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar-weather-info.component.css'
 })
 export class SidebarWeatherInfoComponent {
+  /**
+   * This required input awaits name of the svg icon, which must located in the src/app/assets/sidebar_Weather_Info_Icons
+   * folder and file extension is not required.
+   *
+   * Usage Example:
+   * @example
+   * <app-sidebar-weather-info iconName="cloud_sidebar_info">Mostly Cloudy</app-sidebar-weather-info>
+   */
+  iconName = input.required<string>();
 
+  /**
+   * This required input awaits alt text of the icon, which used for img element and intends to improve accessibility
+   * of the web application.
+   *
+   * Usage Example:
+   * @example
+   * <app-sidebar-weather-info iconAltText="Raindrop Icon">Rain - 40%</app-sidebar-weather-info>
+   */
+  iconAltText = input.required<string>();
+
+  /**
+   * This computed signal automatically computes and returns full path of the input icon to dynamically add desired
+   * icons in the component.
+   */
+  getIconPath = computed<string>(() => `assets/sidebar_Weather_Info_Icons/${this.iconName()}.svg`);
 }
