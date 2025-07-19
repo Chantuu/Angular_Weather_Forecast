2efc3bc500db7546a7e6cd59db868fbed9a84234
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-20 00:43:12 +0400

    Implement CitySearchBarComponent Functionality

--- src/app/components/ui/city-search-bar/city-search-bar.component.ts
+++ src/app/components/ui/city-search-bar/city-search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject, output, signal} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 @Component({
@@ -8,5 +8,26 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './city-search-bar.component.css'
 })
 export class CitySearchBarComponent {
+  /**
+   * This signal is used to save text input element content.
+   */
+  inputText = signal<string>("");
 
+  /**
+   * This event emits text input element content to the parent container.
+   */
+  onSubmit = output<string>();
+
+  /**
+   * This function is responsible for validating and sending text input content to parent
+   * container.
+   */
+  async onSubmitPerform() {
+    if (!this.inputText()) {
+      alert("Please enter a valid city name");
+    }
+    else {
+      this.onSubmit.emit(this.inputText());
+    }
+  }
 }
