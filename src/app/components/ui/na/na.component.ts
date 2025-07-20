a371df83be2dfe53f00e25c1f9fac21a8403337c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-20 17:56:29 +0400

    Create NaComponent
     Created this component to display special message, when no city is selected in the application.

---
+++ src/app/components/ui/na/na.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-na',
+  imports: [],
+  templateUrl: './na.component.html',
+  styleUrl: './na.component.css'
+})
+export class NaComponent {
+
+}
