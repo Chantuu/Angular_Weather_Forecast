f9b511a2dfb1fb1f82e5e145a62819d41bf3fa14
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-20 22:13:18 +0400

    Create WeatherData Type
     Created this type to contain Open-Meteo APIS response data into one compact object for easy data sharing between components.

---
+++ src/app/utilities/types/weather-data.type.ts
@@ -0,0 +1,32 @@
+/**
+ * This type is used to stored Open-Meteo API data into one object and distribute this data across different components
+ * efficiently.
+ */
+export type WeatherData = {
+    /**
+     * This property contains name of the searched city.
+     */
+    city: string;
+    /**
+     * This property contains all information intended for representing whole day weather data.
+     */
+    daily: {
+        date: Date[],
+        weatherCode: number[],
+        temperatureMax: number[],
+        temperatureMin: number[],
+        uvIndex: number,
+        sunrise: Date,
+        sunset: Date,
+        precipitationProbability: number,
+        windSpeed: number,
+    },
+    /**
+     * This property contains all information intended for representing hourly weather data for one day.
+     */
+    hourly: {
+        temperature: number[],
+        time: Date[],
+        weatherCode: number[],
+    }
+};
