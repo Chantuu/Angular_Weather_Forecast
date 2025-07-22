ebc2a150ba754f92f1cbdcc4a21f941953e7b1f2
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-07-22 15:23:15 +0400

    Create WeatherService
     Created this service, which will be responsible for retrieving and sharing weather data across different components.

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,131 @@
+import { Injectable } from '@angular/core';
+import axios from "axios";
+import {WeatherData} from "../utilities/types/weather-data.type";
+import {BehaviorSubject} from "rxjs";
+
+/**
+ * This service class is responsible for retrieving and managing Open-Meteo APIs weather data for different components
+ * across the application.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  /**
+   * RxJS BehaviorSubject property, which is used to save current weather data in BehaviorSubject object for ability
+   * to share this data with any component injecting this service.
+   */
+  private sharedWeatherDataSubject = new BehaviorSubject<WeatherData | null>(null);
+
+  /**
+   * RxJS Observable property, which is used in desired components as an alternative way to get weather data. Note, that
+   * this method is recommended to use in components, where getWeatherData() method won't be used.
+   */
+  sharedWeatherData$ = this.sharedWeatherDataSubject.asObservable();
+
+  /**
+   * This method is used to update contents of the sharedWeatherDataSubject with the provided weatherData parameter.
+   *
+   * @param weatherData - Desired WeatherData object containing up-to-date weather information.
+   */
+  setSharedWeatherData(weatherData: WeatherData) {
+    this.sharedWeatherDataSubject.next(weatherData);
+  }
+
+  /**
+   * This generic private method is used to extract hourly weather data for first day from the Open-Meteo API response.
+   *
+   * @param desiredHourlyWeatherDataArray - Array containing desired hourly data from API response.
+   */
+  private extractHourlyWeatherDataFromResponseArrays<T>(desiredHourlyWeatherDataArray: T[]): T[] {
+    // This line is enough simply, because API returns hourly weather data for specified days too and using this method
+    // only returns hourly weather data of the first dat.
+    return desiredHourlyWeatherDataArray.slice(0, 24);
+  }
+
+  /**
+   * This private method is used to convert date/time string data from Open-Meteo API response into the date objects.
+   *
+   * @param timeArray - Array containing date/time strings.
+   */
+  private convertTimeToDateObject(timeArray: string[]) {
+    const convertedDateArray: Date[] = [];
+
+    for (let i = 0; i < timeArray.length; i++) {
+      convertedDateArray.push(new Date(timeArray[i]));
+    }
+
+    return convertedDateArray;
+  }
+
+  /**
+   * This private async method is responsible for converting city name to the geocode using Open-Meteos API.
+   *
+   * @param cityName - Desired city to be converted into geocodes.
+   * @throws Error - throws error if city with that name was not found or there were connection issues with API.
+   * @Warning - This method may be contained inside try/catch block, as this method has probability to throw an error.
+   */
+  private async convertCityNameToLatLng(cityName: string) {
+    const response = await axios({
+      method: 'GET',
+      url: `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en`,
+    });
+
+    // If city was successfully found
+    if (response.data.results) {
+      return {
+        cityName: response.data.results[0].name,
+        latitude: response.data.results[0].latitude,
+        longitude: response.data.results[0].longitude,
+      };
+    }
+    else {
+      throw new Error(`Unable to find city ${cityName}`);
+    }
+  }
+
+  /**
+   * This async method is used to get full weather data for desired city using the Open-Meteo API.
+   *
+   * @param cityName - Desired city to get weather data for.
+   * @return Promise<WeatherData> - Promise containing WeatherData object, where all weather related information is
+   * saved compactly.
+   * @Warning - This method must be contained inside try/catch block, as this method has probability to throw an error.
+   */
+  async getWeatherData(cityName: string): Promise<WeatherData> {
+    const cityGeocode = await this.convertCityNameToLatLng(cityName);
+
+    const response = await axios({
+      method: 'GET',
+      url: `https://api.open-meteo.com/v1/forecast?latitude=${cityGeocode.latitude}&longitude=${cityGeocode.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,wind_speed_10m_max,precipitation_probability_mean&hourly=weather_code,temperature_2m&timezone=auto`
+    });
+
+    const weatherDataResult: WeatherData = {
+      city: cityGeocode.cityName,
+      daily: {
+        date: this.convertTimeToDateObject(response.data.daily.time), // Converts date strings to Date objects.
+        weatherCode: response.data.daily.weather_code,
+        temperatureMax: response.data.daily.temperature_2m_max,
+        temperatureMin: response.data.daily.temperature_2m_min,
+        uvIndex: response.data.daily.uv_index_max,
+        sunrise: this.convertTimeToDateObject(response.data.daily.sunrise)[0], // Convert and Get Sunrise time for first day
+        sunset: this.convertTimeToDateObject(response.data.daily.sunset)[0], // Convert and Get Sunset time for first day
+        precipitationProbability: response.data.daily.precipitation_probability_mean[0], // Get precipitation probability for first day
+        windSpeed: response.data.daily.wind_speed_10m_max[0], // Get wind speed for first day
+      },
+      hourly: {
+        // Hourly temperature data is extracted for one day
+        temperature: this.extractHourlyWeatherDataFromResponseArrays<number>(response.data.hourly.temperature_2m),
+        // Hourly time data is extracted for one day and then converted into the Date objects.
+        time: this.convertTimeToDateObject(this.extractHourlyWeatherDataFromResponseArrays<string>(response.data.hourly.time)),
+        // Hourly weather code is extracted for one day
+        weatherCode: this.extractHourlyWeatherDataFromResponseArrays<number>(response.data.hourly.weather_code),
+      }
+    }
+
+    // sharedWeatherData behaviorSubject is updated with recent WeatherData object which is necessary to share weather
+    // data to different components in alternative way.
+    this.setSharedWeatherData(weatherDataResult);
+    return weatherDataResult;
+  }
+}
