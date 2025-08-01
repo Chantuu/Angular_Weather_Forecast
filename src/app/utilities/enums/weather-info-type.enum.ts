bc3b06cc1e9404eef3ed5c2b4f6f203b20c31f19
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-01 20:11:15 +0400

    Create WeatherInfoType Enum
     Created this enum to easily specify to the required components, which type of weather data to display and make codebase refactoring easier.

---
+++ src/app/utilities/enums/weather-info-type.enum.ts
@@ -0,0 +1,7 @@
+/**
+ * This enum is used to specify to the components, in which timeline should weather data be displayed.
+ */
+export enum WeatherInfoType {
+    Hourly = 'Hourly',
+    Daily = 'Daily',
+}
