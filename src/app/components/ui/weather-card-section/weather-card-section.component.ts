b8d3d65e231f3530ed3f4a96df2fbec5c474bc2a
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-01 20:04:22 +0400

    Create WeatherCardSection Component

---
+++ src/app/components/ui/weather-card-section/weather-card-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card-section',
+  imports: [],
+  templateUrl: './weather-card-section.component.html',
+  styleUrl: './weather-card-section.component.css'
+})
+export class WeatherCardSectionComponent {
+
+}
