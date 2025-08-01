1e3daacac1a3ac8b91e1430ae6d8873302c228dd
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-01 20:04:38 +0400

    Create WeatherCardContainer Component

---
+++ src/app/components/containers/weather-card-container/weather-card-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card-container',
+  imports: [],
+  templateUrl: './weather-card-container.component.html',
+  styleUrl: './weather-card-container.component.css'
+})
+export class WeatherCardContainerComponent {
+
+}
