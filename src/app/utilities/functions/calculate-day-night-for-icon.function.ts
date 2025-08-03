e7b17a362d8b18afdaf88deec992f9ab8dfb203c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 20:46:08 +0400

    Add Optional Parameter in calculate-day-night-for-icon.function.ts
     Added currentOptonalTime parameter to calculate day/night based on that parameter.

--- src/app/utilities/functions/calculate-day-night-for-icon.function.ts
+++ src/app/utilities/functions/calculate-day-night-for-icon.function.ts
@@ -2,13 +2,22 @@
  * This helper function is used to calculate, if it is night or day based on the current time and return boolean value,
  * where true represents night and false day. Using this, parent function/method will choose corresponding icon.
  *
+ * @param currentOptionalTime - Optional Date value to calculate day/night based on that value.
  * @return Boolean - Boolean, where true represents night and false day.
  */
-export function calculateDayNightForIcon() {
-    // Setup current time and limits for correct calculation
-    const currentTime = new Date(Date.now());
+export function calculateDayNightForIcon(currentOptionalTime?: Date) {
+
+    // Setup current time and limits for correct
+    let currentTime: Date;
     const nightTimeLimit = new Date(Date.now());
     const dayTimeLimit = new Date(Date.now());
+
+    // Set current time based provided time value
+    if (currentOptionalTime) {
+        currentTime = currentOptionalTime;
+    } else {
+        currentTime = new Date(Date.now());
+    }
 
     // Set time limit for night
     nightTimeLimit.setHours(20);
