5613cb32de9f3d8c84876ff85e5e0c6d0bb4454e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 03:04:18 +0400

    Implement UvIndexCard Component Functionality

--- src/app/components/ui/weather-highlight-cards/uv-index-card/uv-index-card.component.ts
+++ src/app/components/ui/weather-highlight-cards/uv-index-card/uv-index-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-uv-index-card',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './uv-index-card.component.css'
 })
 export class UvIndexCardComponent {
-
+  /**
+   * This required input awaits uv index value as a number, which will be displayed in the component.
+   *
+   * Usage example:
+   * @example
+   * <app-uv-index-card [uvIndex]="5" />
+   */
+  uvIndex = input.required<number>();
 }
