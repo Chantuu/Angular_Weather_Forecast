476f5f45db289d53a74316f97afaba65591868a7
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 22:37:02 +0400

    Create UvIndexCard Component

---
+++ src/app/components/ui/weather-highlight-cards/uv-index-card/uv-index-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uv-index-card',
+  imports: [],
+  templateUrl: './uv-index-card.component.html',
+  styleUrl: './uv-index-card.component.css'
+})
+export class UvIndexCardComponent {
+
+}
