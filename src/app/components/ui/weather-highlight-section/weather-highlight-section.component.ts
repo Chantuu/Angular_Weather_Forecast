14f89d742e1afe48fd5019ce372f3591b9c92cba
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 21:33:41 +0400

    Create WeatherHighlightSection Component

---
+++ src/app/components/ui/weather-highlight-section/weather-highlight-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-highlight-section',
+  imports: [],
+  templateUrl: './weather-highlight-section.component.html',
+  styleUrl: './weather-highlight-section.component.css'
+})
+export class WeatherHighlightSectionComponent {
+
+}
