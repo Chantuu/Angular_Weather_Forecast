2ad1660b4c3a900c948839a6f8ab147c62e4ba62
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 21:33:55 +0400

    Create WeatherHighlightCard Component

---
+++ src/app/components/ui/weather-highlight-card/weather-highlight-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-highlight-card',
+  imports: [],
+  templateUrl: './weather-highlight-card.component.html',
+  styleUrl: './weather-highlight-card.component.css'
+})
+export class WeatherHighlightCardComponent {
+
+}
