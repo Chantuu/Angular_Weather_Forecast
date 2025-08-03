e4212bdd08efc5ae57d869779214e651b62b2d6c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 22:37:18 +0400

    Create SunriseSunsetCard Component

---
+++ src/app/components/ui/weather-highlight-cards/sunrise-sunset-card/sunrise-sunset-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sunrise-sunset-card',
+  imports: [],
+  templateUrl: './sunrise-sunset-card.component.html',
+  styleUrl: './sunrise-sunset-card.component.css'
+})
+export class SunriseSunsetCardComponent {
+
+}
