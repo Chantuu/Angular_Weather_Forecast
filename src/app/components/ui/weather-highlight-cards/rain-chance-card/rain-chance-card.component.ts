e27624a9d2130487d96133951724920152c47199
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 22:37:34 +0400

    Create RainChanceCard Component

---
+++ src/app/components/ui/weather-highlight-cards/rain-chance-card/rain-chance-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rain-chance-card',
+  imports: [],
+  templateUrl: './rain-chance-card.component.html',
+  styleUrl: './rain-chance-card.component.css'
+})
+export class RainChanceCardComponent {
+
+}
