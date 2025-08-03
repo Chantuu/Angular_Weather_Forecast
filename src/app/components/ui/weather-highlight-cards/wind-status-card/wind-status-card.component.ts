b409b26e1f32d2a74cb425c8f85f0166439739cb
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-03 22:37:53 +0400

    Create WindStatusCard Component

---
+++ src/app/components/ui/weather-highlight-cards/wind-status-card/wind-status-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wind-status-card',
+  imports: [],
+  templateUrl: './wind-status-card.component.html',
+  styleUrl: './wind-status-card.component.css'
+})
+export class WindStatusCardComponent {
+
+}
