f8d3600c6f68cb8ab8f495b6a5e883e3a9087d50
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 19:28:52 +0400

    Replace Old Components with New Component
     Replaced HeaderCelsiusButton and HeaderFahrenheitButton components with HeaderToggleButton component to avoid code duplication.

---
+++ src/app/components/ui/header-buttons/header-toggle-button/header-toggle-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-toggle-button',
+  imports: [],
+  templateUrl: './header-toggle-button.component.html',
+  styleUrl: './header-toggle-button.component.css'
+})
+export class HeaderToggleButtonComponent {
+
+}
