4436bf26a0ab617a45612226653da540d58a7c0f
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:00:18 +0400

    Implement Basic Functionality in MainContainer Component

--- src/app/components/containers/main-container/main-container.component.ts
+++ src/app/components/containers/main-container/main-container.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {WeatherCardSectionComponent} from "../../ui/weather-card-section/weather-card-section.component";
+import {WeatherInfoType} from "../../../utilities/enums/weather-info-type.enum";
+import {WeatherHighlightSectionComponent} from "../../ui/weather-highlight-section/weather-highlight-section.component";
 
 @Component({
   selector: 'app-main-container',
-  imports: [],
+  imports: [
+    WeatherCardSectionComponent,
+    WeatherHighlightSectionComponent
+  ],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
 export class MainContainerComponent {
-
+  /**
+   * This enum property was declared to be used inside component template to properly display time
+   * based on WeatherInfoType enum.
+   */
+  protected readonly WeatherInfoType = WeatherInfoType;
 }
