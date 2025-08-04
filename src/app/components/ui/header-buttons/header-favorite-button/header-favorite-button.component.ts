02548c9959258445a6af65480d20bf27bba37a4a
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:25:53 +0400

    Create HeaderFavoriteButton Component

---
+++ src/app/components/ui/header-buttons/header-favorite-button/header-favorite-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-favorite-button',
+  imports: [],
+  templateUrl: './header-favorite-button.component.html',
+  styleUrl: './header-favorite-button.component.css'
+})
+export class HeaderFavoriteButtonComponent {
+
+}
