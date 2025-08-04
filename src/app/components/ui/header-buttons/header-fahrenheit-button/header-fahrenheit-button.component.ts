d0ad75f15700d444856d9101ee24094c0fa50a93
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:25:43 +0400

    Create HeaderFahrenheitButton Component

---
+++ src/app/components/ui/header-buttons/header-fahrenheit-button/header-fahrenheit-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-fahrenheit-button',
+  imports: [],
+  templateUrl: './header-fahrenheit-button.component.html',
+  styleUrl: './header-fahrenheit-button.component.css'
+})
+export class HeaderFahrenheitButtonComponent {
+
+}
