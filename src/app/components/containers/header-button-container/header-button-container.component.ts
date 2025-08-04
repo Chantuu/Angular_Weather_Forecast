989cfce04d7c70615c49857253f088dfb131350e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:23:48 +0400

    Create HeaderButtonContainer Component

---
+++ src/app/components/containers/header-button-container/header-button-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-button-container',
+  imports: [],
+  templateUrl: './header-button-container.component.html',
+  styleUrl: './header-button-container.component.css'
+})
+export class HeaderButtonContainerComponent {
+
+}
