af72b01bd125885802c3654e1ba9d0a36ac2162b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:25:16 +0400

    Create HeaderCelsiusButton Component

---
+++ src/app/components/ui/header-buttons/header-celcius-button/header-celsius-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-celsius-button',
+  imports: [],
+  templateUrl: './header-celsius-button.component.html',
+  styleUrl: './header-celsius-button.component.css'
+})
+export class HeaderCelsiusButtonComponent {
+
+}
