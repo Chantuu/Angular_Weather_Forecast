94d89878b1ffd410e3c344dfb3eef261697b6ddb
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 15:15:58 +0400

    Implement Full Functionality in MainContainer Component

--- src/app/components/containers/main-container/main-container.component.ts
+++ src/app/components/containers/main-container/main-container.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {WeatherCardSectionComponent} from "../../ui/weather-card-section/weather-card-section.component";
 import {WeatherInfoType} from "../../../utilities/enums/weather-info-type.enum";
 import {WeatherHighlightSectionComponent} from "../../ui/weather-highlight-section/weather-highlight-section.component";
+import {WeatherService} from "../../../services/weather.service";
+import {WeatherData} from "../../../utilities/types/weather-data.type";
 
 @Component({
   selector: 'app-main-container',
@@ -12,7 +14,26 @@ import {WeatherHighlightSectionComponent} from "../../ui/weather-highlight-secti
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
-export class MainContainerComponent {
+export class MainContainerComponent implements OnInit {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  /**
+   * This life-hook method is used to set up RxJS subscription with WeatherService,
+   * which will return WeatherData object containing Open-Meteo API response data and
+   * set it in this component as property. This subscription will automatically
+   * update this property with new value when a change occurs.
+   */
+  ngOnInit(): void {
+    this.weatherService.sharedWeatherData$.subscribe(data => {
+      this.sharedWeatherData.set(data);
+    });
+  }
+
+  /**
+   * This property is signal, which is used to save WeatherData shared from WeatherService using RxJS.
+   */
+  sharedWeatherData = signal<WeatherData | null>(null);
+
   /**
    * This enum property was declared to be used inside component template to properly display time
    * based on WeatherInfoType enum.
