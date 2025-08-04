dde4cf7c2f6b9a1e4022dffecf30faefb1231b7c
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-04 11:20:01 +0400

    Implement WindStatusCard Component Functionality

--- src/app/components/ui/weather-highlight-cards/wind-status-card/wind-status-card.component.ts
+++ src/app/components/ui/weather-highlight-cards/wind-status-card/wind-status-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-wind-status-card',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './wind-status-card.component.css'
 })
 export class WindStatusCardComponent {
-
+  /**
+   * This required input awaits wind speed value of number type to be displayed inside component.
+   *
+   * Usage example:
+   * @example
+   * <app-wind-status-card [windStatus]="15" />
+   */
+  windStatus = input.required<number>()
 }
