8cadb4ca9bca2ed945a90f0934ce65ae8823ac62
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-12 04:33:30 +0400

    Create HeaderToggleButtonType Enum

---
+++ src/app/utilities/enums/header-toggle-button-type.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * This enum is used to programmatically differentiate toggle buttons, which are used to display temperature between
+ * Celsius and Fahrenheit.
+ */
+export enum HeaderToggleButtonType {
+    celsius = 0,
+    fahrenheit = 1,
+}
