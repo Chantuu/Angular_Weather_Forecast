123ff30134a2acfca0c72babd7433affa0534cc3
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-14 01:40:07 +0400

    Implement TemperatureFormatPipe

---
+++ src/app/pipes/temperature-format.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {TemperatureFormatService} from "../services/temperature-format.service";
+import {HeaderToggleButtonType} from "../utilities/enums/header-toggle-button-type.enum";
+
+/**
+ * This pipe is used to display temperature in Celsius or in Fahrenheit. TemperatureFormatService is used to display
+ * temperature conditionally in either of these formats.
+ */
+@Pipe({
+  name: 'temperatureFormat',
+  pure: false
+})
+export class TemperatureFormatPipe implements PipeTransform {
+  constructor(private readonly temperatureFormatService: TemperatureFormatService) {
+  }
+
+  transform(value: number): string {
+    console.log(this.temperatureFormatService.getTemperatureFormat());
+    if (this.temperatureFormatService.getTemperatureFormat() === HeaderToggleButtonType.fahrenheit) {
+      return `${Math.round(((value * (9 / 5)) + 32) * 100) / 100}℉`; // Fahrenheit is rounded to 2 decimals
+    } else {
+      return `${value}℃`;
+    }
+  }
+}
