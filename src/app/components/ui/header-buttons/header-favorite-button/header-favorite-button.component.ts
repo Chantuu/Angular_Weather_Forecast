5a0f64a26859a95470473cbfcb2322436319322e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-13 23:04:17 +0400

    Implement HeaderFavoriteButton Basic Functionality

--- src/app/components/ui/header-buttons/header-favorite-button/header-favorite-button.component.ts
+++ src/app/components/ui/header-buttons/header-favorite-button/header-favorite-button.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-header-favorite-button',
   imports: [],
   templateUrl: './header-favorite-button.component.html',
-  styleUrl: './header-favorite-button.component.css'
+  styleUrl: './header-favorite-button.component.css',
+  host: {
+    "[class.buttonActivated]": "buttonActivated()"
+  }
 })
 export class HeaderFavoriteButtonComponent {
-
+  /**
+   * This input awaits boolean value, which controls visual of the button and tells user when favorite city is saved
+   * to the list.
+   *
+   * Usage example:
+   * @example
+   * <app-header-favorite-button [buttonActivated]="false" />
+   */
+  buttonActivated = input.required<boolean>();
 }
