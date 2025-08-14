c392ba9a662462357f3818fcc24038e30b2a458e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-14 23:36:03 +0400

    Create CityListTab Component

---
+++ src/app/components/ui/city-list-tab/city-list-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-list-tab',
+  imports: [],
+  templateUrl: './city-list-tab.component.html',
+  styleUrl: './city-list-tab.component.css'
+})
+export class CityListTabComponent {
+
+}
