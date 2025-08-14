f43dfcabed22bc7b6936e6794df1e439b0f0ee68
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-08-14 23:32:40 +0400

    Create CityListItem Component

---
+++ src/app/components/ui/city-list-item/city-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-list-item',
+  imports: [],
+  templateUrl: './city-list-item.component.html',
+  styleUrl: './city-list-item.component.css'
+})
+export class CityListItemComponent {
+
+}
